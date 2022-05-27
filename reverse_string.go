bf44ffc988215f5d3c60bc031f0a0852b180f5e0
Geepasx <zh.baupro@icloud.com>
2022-05-27 21:58:03 +0600

    feat: first solution

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,12 @@
 package reverse_string
 
+import "fmt"
+
 func ReverseString(input string) (output string) {
-	// solution goes here
+
+	for i := len(input) - 1; i >= 0; i-- {
+		output += string(input[i])
+		fmt.Println(string(input[i]))
+	}
 	return output
 }
